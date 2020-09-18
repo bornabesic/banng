73059a0398069c1a433ef497c82565e989fc0e23
Borna Bešić <besic.borna@gmail.com>
2020-09-18 17:03:07 +0200

    Add indexing and search to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 
 #include <array.hpp>
+#include <index.hpp>
 #include <iostream>
 
 int main(void) {
@@ -17,7 +18,20 @@ int main(void) {
     array.data[2][0] = 5;
     array.data[2][1] = 6;
 
+    std::cout << "Data:" << '\n';
     print_array_2d(array);
+
+    index<float> *i = build_index(array);
+
+    float query[2] = {0};
+    array_1d<float> query_array = as_array_1d(query, d);
+    std::cout << "Query:" << '\n';
+    print_array_1d(query_array);
+
+    array_1d<float> result_array = search_index(i, query_array);
+    std::cout << "Result:" << '\n';
+    print_array_1d(result_array);
+
     free_array_2d(array);
     return 0;
 }

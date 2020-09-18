777efefb15e10292517ab8a4058bae4f6e8d0596
Borna Bešić <besic.borna@gmail.com>
2020-09-18 16:57:19 +0200

    Add 1D array interface

--- include/array.hpp
+++ include/array.hpp
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <iostream>
 
+// --------------------------------- 2D Array ---------------------------------
+
 template <typename T>
 struct array_2d {
     T **data;
@@ -15,7 +17,7 @@ array_2d<T> allocate_array_2d(const unsigned int rows,
 
     T *data = new T[rows * cols];
     T **row_ptrs = new T *[rows];
-    for (int i = 0; i < rows; ++i)
+    for (unsigned int i = 0; i < rows; ++i)
         row_ptrs[i] = data + i * cols;
 
     assert(&row_ptrs[rows - 1][cols - 1] - &row_ptrs[0][0] + 1 == rows * cols);
@@ -24,8 +26,8 @@ array_2d<T> allocate_array_2d(const unsigned int rows,
 
 template <typename T>
 void print_array_2d(const struct array_2d<T> &array) {
-    for (int i = 0; i < array.rows; ++i) {
-        for (int j = 0; j < array.cols; ++j)
+    for (unsigned int i = 0; i < array.rows; ++i) {
+        for (unsigned int j = 0; j < array.cols; ++j)
             std::cout << array.data[i][j] << ' ';
         std::cout << '\n';
     }
@@ -35,4 +37,24 @@ template <typename T>
 void free_array_2d(const struct array_2d<T> &array) {
     delete array.data[0];
     delete array.data;
+}
+
+// --------------------------------- 1D Array ---------------------------------
+
+template <typename T>
+struct array_1d {
+    T *data;
+    unsigned int length;
+};
+
+template <typename T>
+struct array_1d<T> as_array_1d(T *data, unsigned int length) {
+    return {data, length};
+}
+
+template <typename T>
+void print_array_1d(const struct array_1d<T> &array) {
+    for (int i = 0; i < array.length; ++i)
+        std::cout << array.data[i] << ' ';
+    std::cout << '\n';
 }

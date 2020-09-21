2b34034635c79adab2ab46697f55f0ff4dc0e980
Borna Bešić <besic.borna@gmail.com>
2020-09-21 17:53:35 +0200

    Allocate a random array in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,26 @@
 
+#include <chrono>
+#include <iostream>
 
 #include <array.hpp>
 #include <index.hpp>
-#include <iostream>
+#include <random.hpp>
 
 int main(void) {
-    int n = 3;
-    int d = 2;
+    constexpr unsigned int n = 1000000;
+    constexpr unsigned int d = 32;
     struct array_2d<float> array = allocate_array_2d<float>(n, d);
 
-    array.data[0][0] = 1;
-    array.data[0][1] = 2;
-    //
-    array.data[1][0] = 3.14;
-    array.data[1][1] = 4;
-    //
-    array.data[2][0] = 5;
-    array.data[2][1] = 6;
-
-    std::cout << "Data:" << '\n';
-    print_array_2d(array);
+    auto t_start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < n * d; ++i)
+        array.data[0][i] = random_normal();
+    std::chrono::duration<float> t = std::chrono::high_resolution_clock::now() - t_start;
+    std::cout << "Allocation of " << n << " x " << d << " array took " << t.count() << " s" << '\n';
+    std::cout << n * d * sizeof(float) / 1024.f / 1024.f << " MB" << '\n';
 
     index<float> *i = build_index(array);
 
-    float query[2] = {0};
+    float query[d] = {0};
     array_1d<float> query_array = as_array_1d(query, d);
     std::cout << "Query:" << '\n';
     print_array_1d(query_array);

02ae832c8b19270a001dba84e4e916b0f07d9f7c
Borna Bešić <besic.borna@gmail.com>
2020-09-21 18:25:38 +0200

    Update main()

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,33 @@
 #include <array.hpp>
 #include <index.hpp>
 #include <random.hpp>
+#include <time.hpp>
 
 int main(void) {
     constexpr unsigned int n = 1000000;
     constexpr unsigned int d = 32;
     struct array_2d<float> array = allocate_array_2d<float>(n, d);
 
-    auto t_start = std::chrono::high_resolution_clock::now();
+    Stopwatch stopwatch;
+
+    stopwatch.checkpoint();
     for (int i = 0; i < n * d; ++i)
         array.data[0][i] = random_normal();
-    std::chrono::duration<float> t = std::chrono::high_resolution_clock::now() - t_start;
-    std::cout << "Allocation of " << n << " x " << d << " array took " << t.count() << " s" << '\n';
+    std::cout << "Random data generation for " << n << " x " << d << " array took " << stopwatch.checkpoint() << " s" << '\n';
     std::cout << n * d * sizeof(float) / 1024.f / 1024.f << " MB" << '\n';
 
+    stopwatch.checkpoint();
     index<float> *i = build_index(array);
+    std::cout << "Building the index took " << stopwatch.checkpoint() << " s" << '\n';
 
     float query[d] = {0};
     array_1d<float> query_array = as_array_1d(query, d);
     std::cout << "Query:" << '\n';
     print_array_1d(query_array);
 
+    stopwatch.checkpoint();
     array_1d<float> result_array = search_index(i, query_array);
+    std::cout << "Querying the index took " << stopwatch.checkpoint() << " s" << '\n';
     std::cout << "Result:" << '\n';
     print_array_1d(result_array);
 

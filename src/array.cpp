3dc0f3324c5659e66fdb376dcdddaeb0f084f594
Borna Bešić <besic.borna@gmail.com>
2020-09-29 19:55:00 +0200

    Fix the buffer pointer

--- src/array.cpp
+++ src/array.cpp
@@ -12,10 +12,10 @@ PYBIND11_MODULE(banng, m) {
     py::class_<Array2d<float>>(m, "Array2d", py::buffer_protocol())
         .def_buffer([](Array2d<float> &a) -> py::buffer_info {
             return py::buffer_info(
-                a.data,                                 /* Pointer to buffer */
+                *a.data,                                /* Pointer to buffer */
                 sizeof(float),                          /* Size of one scalar */
                 py::format_descriptor<float>::format(), /* Python struct-style
-                                                        format descriptor */
+                                                           format descriptor */
                 2,                       /* Number of dimensions */
                 {a.rows, a.cols},        /* Buffer dimensions */
                 {sizeof(float) * a.cols, /* Strides (in bytes) for each index */

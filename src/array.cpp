7a39edbe9fb3eccf9863737fe65ead303632fa04
Borna Bešić <besic.borna@gmail.com>
2020-09-29 19:52:25 +0200

    Implement from_numpy() for converting np.array to Array2d<float>

--- src/array.cpp
+++ src/array.cpp
@@ -1,4 +1,7 @@
 
+#include <cassert>
+#include <iostream>
+
 #include <pybind11/pybind11.h>
 
 #include <array.hpp>
@@ -19,5 +22,31 @@ PYBIND11_MODULE(banng, m) {
                  sizeof(float)});
         })
         .def("allocate", &Array2d<float>::allocate)
-        .def("free", &Array2d<float>::free);
+        .def("free", &Array2d<float>::free)
+        .def("print", &Array2d<float>::print)
+        .def("from_numpy", [](py::buffer buffer, bool verbose = false) -> Array2d<float> {
+            const py::buffer_info info = buffer.request();
+
+            assert(py::format_descriptor<float>::format() == info.format);
+            assert(info.ndim == 2);
+
+            const unsigned int rows = info.shape[0];
+            const unsigned int cols = info.shape[1];
+            assert(info.strides[0] == cols * sizeof(float));
+            assert(info.strides[1] == sizeof(float));
+
+            float *data = (float*) info.ptr;
+            float **row_ptrs = new float*[rows];
+            for (unsigned int i = 0; i < rows; ++i)
+                row_ptrs[i] = data + cols * i;
+
+            if (verbose) {
+                std::cout << "Format: " << info.format << '\n';
+                std::cout << "Shape: " << info.shape[0] << " x " << info.shape[1] << '\n';
+                std::cout << "Strides: " << info.strides[0] << ", " << info.strides[1] << '\n';
+                std::cout << "Size: " << info.size << '\n';
+            }
+
+            return {row_ptrs, rows, cols};
+        }, py::arg("verbose") = false);
 }

1a2550bddbe0904fac8cf0794eff4e20a3ae75f2
Borna Bešić <besic.borna@gmail.com>
2020-09-29 20:40:55 +0200

    Define EXPORT_ARRAY_2D macro

--- src/array.cpp
+++ src/array.cpp
@@ -8,46 +8,51 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(banng, m) {
-    py::module m_float = m.def_submodule("float");
-    py::class_<Array2d<float>>(m_float, "Array2d", py::buffer_protocol())
-        .def_buffer([](Array2d<float> &a) -> py::buffer_info {
-            return py::buffer_info(
-                *a.data,                                /* Pointer to buffer */
-                sizeof(float),                          /* Size of one scalar */
-                py::format_descriptor<float>::format(), /* Python struct-style
-                                                           format descriptor */
-                2,                       /* Number of dimensions */
-                {a.rows, a.cols},        /* Buffer dimensions */
-                {sizeof(float) * a.cols, /* Strides (in bytes) for each index */
-                 sizeof(float)});
-        })
-        .def("allocate", &Array2d<float>::allocate)
-        .def("free", &Array2d<float>::free)
-        .def("print", &Array2d<float>::print)
-        .def("from_numpy", [](py::buffer buffer, bool verbose = false) -> Array2d<float> {
-            const py::buffer_info info = buffer.request();
+#define EXPORT_ARRAY_2D(module, type) {                                                         \
+    py::module m_sub = module.def_submodule(#type);                                             \
+    py::class_<Array2d<type>>(m_sub, "Array2d", py::buffer_protocol())                          \
+        .def_buffer([](Array2d<type> &a) -> py::buffer_info {                                   \
+            return py::buffer_info(                                                             \
+                *a.data,                               /* Pointer to buffer */                  \
+                sizeof(type),                          /* Size of one scalar */                 \
+                py::format_descriptor<type>::format(), /* Python struct-style /                 \
+                                                        format descriptor */                    \
+                2,                       /* Number of dimensions */                             \
+                {a.rows, a.cols},        /* Buffer dimensions */                                \
+                {sizeof(type) * a.cols, /* Strides (in bytes) for each index */                 \
+                sizeof(type)});                                                                 \
+        })                                                                                      \
+        .def("allocate", &Array2d<type>::allocate)                                              \
+        .def("free", &Array2d<type>::free)                                                      \
+        .def("print", &Array2d<type>::print)                                                    \
+        .def("from_numpy", [](py::buffer buffer, bool verbose = false) -> Array2d<type> {       \
+            const py::buffer_info info = buffer.request();                                      \
+                                                                                                \
+            assert(py::format_descriptor<type>::format() == info.format);                       \
+            assert(info.ndim == 2);                                                             \
+                                                                                                \
+            const unsigned int rows = info.shape[0];                                            \
+            const unsigned int cols = info.shape[1];                                            \
+            assert(info.strides[0] == cols * sizeof(type));                                     \
+            assert(info.strides[1] == sizeof(type));                                            \
+                                                                                                \
+            type *data = (type*) info.ptr;                                                      \
+            type **row_ptrs = new type*[rows];                                                  \
+            for (unsigned int i = 0; i < rows; ++i)                                             \
+                row_ptrs[i] = data + cols * i;                                                  \
+                                                                                                \
+            if (verbose) {                                                                      \
+                std::cout << "Format: " << info.format << '\n';                                 \
+                std::cout << "Shape: " << info.shape[0] << " x " << info.shape[1] << '\n';      \
+                std::cout << "Strides: " << info.strides[0] << ", " << info.strides[1] << '\n'; \
+                std::cout << "Size: " << info.size << '\n';                                     \
+            }                                                                                   \
+                                                                                                \
+            return {row_ptrs, rows, cols};                                                      \
+        }, py::arg("verbose") = false);                                                         \
+}
 
-            assert(py::format_descriptor<float>::format() == info.format);
-            assert(info.ndim == 2);
-
-            const unsigned int rows = info.shape[0];
-            const unsigned int cols = info.shape[1];
-            assert(info.strides[0] == cols * sizeof(float));
-            assert(info.strides[1] == sizeof(float));
-
-            float *data = (float*) info.ptr;
-            float **row_ptrs = new float*[rows];
-            for (unsigned int i = 0; i < rows; ++i)
-                row_ptrs[i] = data + cols * i;
-
-            if (verbose) {
-                std::cout << "Format: " << info.format << '\n';
-                std::cout << "Shape: " << info.shape[0] << " x " << info.shape[1] << '\n';
-                std::cout << "Strides: " << info.strides[0] << ", " << info.strides[1] << '\n';
-                std::cout << "Size: " << info.size << '\n';
-            }
-
-            return {row_ptrs, rows, cols};
-        }, py::arg("verbose") = false);
+PYBIND11_MODULE(banng, module) {
+    EXPORT_ARRAY_2D(module, float);
+    EXPORT_ARRAY_2D(module, double);
 }

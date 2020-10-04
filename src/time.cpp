8fbda7f0604454ecb49385d24ef1f2610b046726
Borna Bešić <besic.borna@gmail.com>
2020-10-04 15:03:10 +0200

    Add license headers

--- src/time.cpp
+++ src/time.cpp
@@ -1,3 +1,10 @@
+// BANNG (c) by Borna Bešić
+// 
+// BANNG is licensed under a
+// Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+// 
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 
 #include <chrono>
 
